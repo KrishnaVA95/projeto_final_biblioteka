fed4491341ac90cb35b0d3695e0505e1504da791
Krishna <krishnavarela.a@gmail.com>
2023-07-04 10:31:43 -0300

    feat: generics view

--- loans/views.py
+++ loans/views.py
@@ -1,16 +1,13 @@
-from rest_framework.views import APIView, Request, Response, status
 from .models import Loan
-from django.shortcuts import get_object_or_404
 from .serializers import LoanSerializer
+from rest_framework import generics
+
+class LoanView(generics.ListCreateAPIView):
+    queryset= Loan.objects.all()
+    serializer_class= LoanSerializer
 
 
-class LoanView(APIView):
-    def get(self, request: Request) -> Response:
-        loans = Loan.objects.all()
-        serializer = LoanSerializer(loans, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    def post(self, request: Request) -> Response:
-        serializer = LoanSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status.HTTP_201_CREATED)
+class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= Loan.objects.all()
+    serializer_class= LoanSerializer
+    
