6fe300be3a9df2abea61a241ad160e9d1f931bc2
carlosgaraujo <carlosguilherme1991@gmail.com>
2023-07-05 09:45:10 -0300

    feat:account

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,5 @@ from accounts.serializers import AccountSerializer
 class AccountView(APIView):
     def get(self, request: Request) -> Response:
         accounts = Accounts.objects.all()
-
         serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
