528f99ee0a43f727a714c3cea8beebdd5f00791a
Kaique Amorim <106697668+KaiqueVasc@users.noreply.github.com>
2023-07-05 15:57:42 -0300

    fixed feat/books IntegerField

--- books/models.py
+++ books/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=127)
     author = models.CharField(max_length=127)
-    number_page = models.IntegerField(validators=[min(4)])
+    number_page = models.IntegerField()
     description = models.TextField()
     cover = models.CharField(max_length=255)
-    published = models.IntegerField(validators=[min(4)])
-    number_copy = models.IntegerField(validators=[min(4)])
-    copies_available = models.IntegerField(validators=[min(2)])
+    published = models.IntegerField()
+    number_copy = models.IntegerField()
+    copies_available = models.IntegerField()
 
 
 
