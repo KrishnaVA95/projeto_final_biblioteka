92139c4f8c0404fe9b0e7cbb90b0686a77954ec5
carlosgaraujo <carlosguilherme1991@gmail.com>
2023-07-05 09:27:06 -0300

    models,serializers e views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
 from rest_framework_simplejwt import views as jwt_views
+
+from . import views
 
 urlpatterns = [
     path("accounts/", views.AccountView.as_view()),
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from rest_framework import generics
-# Create your views here.
-class AccountView(generics.CreateAPIView):
-    ...
+from rest_framework.views import APIView, Request, Response, status
+
+from accounts.models import Accounts
+from accounts.serializers import AccountSerializer
+
+
+class AccountView(APIView):
+    def get(self, request: Request) -> Response:
+        accounts = Accounts.objects.all()
+
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
