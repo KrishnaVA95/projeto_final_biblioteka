db43020ef4683f670120721d018d4a6756a646bf
Kaique Amorim <106697668+KaiqueVasc@users.noreply.github.com>
2023-07-06 09:49:08 -0300

    feat/books Add url to .core

--- books/models.py
+++ books/models.py
@@ -9,12 +9,12 @@ class Book(models.Model):
     published = models.IntegerField()
     number_copy = models.IntegerField()
     copies_available = models.IntegerField()
-    publishing_company_id = models.ForeignKey(
-        "books.Publishing_company", related_name="books"
-    )
-    user = models.ManyToManyField(
-        "accounts.Account", related_name="books"
-    )
+    ##publishing_company_id = models.ForeignKey(
+    "books.Publishing_company"##, related_name="books"
+    ##)
+    ##user = models.ManyToManyField(
+    "accounts.Account", ##related_name="books"
+   ## )
 
 class Publishing_company(models.Model):
     name = models.CharField(max_length=50)
