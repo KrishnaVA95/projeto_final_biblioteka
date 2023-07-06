eadccd4e869354b5c8b4d81b9ad599af5a2b444b
Krishna <krishnavarela.a@gmail.com>
2023-07-06 09:43:34 -0300

    feat: workalendar, views

--- loans/views.py
+++ loans/views.py
@@ -3,11 +3,23 @@ from .serializers import LoanSerializer
 from rest_framework import generics
 
 class LoanView(generics.ListCreateAPIView):
+    # apenas staff ou admin
     queryset= Loan.objects.all()
     serializer_class= LoanSerializer
 
-
-class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
+class LoanDetailView(generics.RetrieveAPIView):
+    # apenas staff, admin ou user autenticado
     queryset= Loan.objects.all()
     serializer_class= LoanSerializer
-    
+
+class LoanDetailUpdateView(generics.UpdateAPIView):
+    # apenas staff ou admin 
+    queryset= Loan.objects.all()
+    serializer_class= LoanSerializer
+    ...
+
+class LoanDetailDestroyView(generics.DestroyAPIView):
+    # apenas staff ou admin 
+    queryset= Loan.objects.all()
+    serializer_class= LoanSerializer
+    ...
