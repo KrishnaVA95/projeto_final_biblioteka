35c5e8854d83d0b64c94954bc1c9ac4134bc27a3
Kaique Amorim <106697668+KaiqueVasc@users.noreply.github.com>
2023-07-07 12:51:34 -0300

    feat/books views complete

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("books/", views.BookView.as_view()),
+    path("books/<int:pk>/", views.BookDetailView.as_view()),
+    path("publishing_company/", views.PublishCompanyView.as_view()),
+    path("publishing_company/<int:pk>/", views.PublishCompanyDetailView.as_view())
 ]
